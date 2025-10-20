bffc5a0ba22395ce4ac909ce4a027b403209d6b5
trevolcanm10 <denilson.morales@unmsm.edu.pe>
2025-10-20 03:48:59 -0400

    Vista-FiltradoAvanzado

---
+++ view/ListPokemon/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>DeniCode</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Filtro Avanzando</h1> 
+        <div class="row">
+            <div class="col-md-3">
+                <div class="list-group">
+                    <h4>Tipo</h4>
+                    <div>
+                        <div>
+                            <div class="list-group-item checkbox">
+                            <label ><input type="checkbox" value="">Fire</label>
+                        </div>
+
+                        <div class="list-group-item checkbox">
+                            <label ><input type="checkbox" value="">Water</label>
+                        </div>
+
+                        <div class="list-group-item checkbox">
+                            <label ><input type="checkbox" value="">Grass</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="row">
+                    <div class="col-sm-4 col-lg-3 col-md-3">
+                        <h1>a</h1>
+                    </div>    
+                </div>
+            </div>
+        </div>       
+    </div>
+    
+</body>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+<script src="listpokemon.js"></script>
+</html>
