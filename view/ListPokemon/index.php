9bfa6109915a8f0c0ffd38cc5d1b43fdb8cd9894
trevolcanm10 <denilson.morales@unmsm.edu.pe>
2025-10-21 01:16:32 -0400

    Estilos para visualizar la lista de los pokemones

--- view/ListPokemon/index.php
+++ view/ListPokemon/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../assets/style.css" >
     <title>DeniCode</title>
 </head>
 <body>
@@ -14,8 +15,7 @@
                 <div class="list-group">
                     <h4>Tipo</h4>
                     <div>
-                        <div>
-                            <div class="list-group-item checkbox">
+                        <div class="list-group-item checkbox">
                             <label ><input type="checkbox" value="">Fire</label>
                         </div>
 
@@ -29,16 +29,35 @@
                     </div>
                 </div>
             </div>
+
             <div class="col-md-9">
                 <div class="row">
                     <div class="col-sm-4 col-lg-3 col-md-3">
+                        <div class="pokemon">
+                            <img src="https://img.pokemondb.net/sprites/black-white/anim/normal/bulbasaur.gif" alt="" class="img-responsive">
+                            <p>001</p>
+                            <h4>Bulbasor</h4>
+                            <h5>Grass</h5>
+                            <p>Ataque:100</p>
+                            <p>Defensa:100</p>
+                        </div>
+                    </div>
+                    
+                    <div class="col-sm-4 col-lg-3 col-md-3">
                         <h1>a</h1>
-                    </div>    
+                    </div>
+                    
+                    <div class="col-sm-4 col-lg-3 col-md-3">
+                        <h1>a</h1>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-3 col-md-3">
+                        <h1>a</h1>
+                    </div>
                 </div>
             </div>
         </div>       
     </div>
-    
 </body>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
